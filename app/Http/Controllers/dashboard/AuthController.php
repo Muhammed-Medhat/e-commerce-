942af6ee2a32f2853f73f94deee74158212e8b53
Muhammed-Medhat <muhammadmedhat02@gmail.com>
2023-11-19 19:16:58 +0200

    add filter on listing customers

--- app/Http/Controllers/dashboard/AuthController.php
+++ app/Http/Controllers/dashboard/AuthController.php
@@ -8,12 +8,14 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\User;
 use App\Traits\UploadBese64Image;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Support\Facades\File;
+use Illuminate\Validation\Rule;
 
 class AuthController extends Controller
 {
@@ -213,11 +215,69 @@ class AuthController extends Controller
             ], 500);
         }
     }
-
-    function listing() {
+    //get all users customers and admins
+    function listing(Request $request) {
         try {
-            $customers= User::where('is_admin',0)->paginate();
-            return response()->json(['data'=>$customers,'status'=>true]);
+
+            // Validation
+            $validator = Validator::make($request->all(), [
+                'q'=>['string'],
+                'sort_by' => [Rule::in(["a-z","z-a","old","new"])],
+                'filter_by_date_range' => ['json'],
+                'is_admin'=>[Rule::in([0, 1])],
+            ]);
+
+            // valid error message
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+            $customers = User::query();
+
+            if ($request->has('is_admin')) {
+                $request->is_admin == 1 ? $customers->where('is_admin',1) : $customers->where('is_admin',0);
+            }
+
+            if(isset($request->filter_by_date_range)){
+                $filter_by_date_range = json_decode($request->filter_by_date_range);
+    
+                $customers->whereBetween('created_at', [
+                    Carbon::parse($filter_by_date_range[0])->format('Y-m-d\TH:i:s.u\Z'),
+                    Carbon::parse($filter_by_date_range[1])->format('Y-m-d\TH:i:s.u\Z'),
+                ]);
+            }
+                // Filter by search
+                if(isset($request->q)){
+                    $query = $request->q;
+                    $customers
+                    ->where('name_ar', 'like', "%{$query}%");
+                }
+    
+            // Sort asc
+            if(isset($request->sort_by) && $request->sort_by == "a-z"){
+                $customers->orderBy("id","asc");
+            }
+    
+            // Sort desc
+            if(isset($request->sort_by) && $request->sort_by == "z-a"){
+                $customers->orderBy("id","desc");
+            }
+    
+            // Filter by date old
+            if(isset($request->sort_by) && $request->sort_by == "old"){
+                $customers->orderBy("created_at","asc");
+            }
+        
+            // Filter by date new
+            if(isset($request->sort_by) && $request->sort_by == "new"){
+                $customers->orderBy("created_at","desc");
+            }
+
+            return response()->json(['data'=>$customers->paginate(), 'status'=>true]);
+
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => false,
